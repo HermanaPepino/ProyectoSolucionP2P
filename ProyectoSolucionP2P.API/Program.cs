f98ef753678387e0a332eaa773a1db145b210d64
Leonardo Diaz <23100503@ue.edu.pe>
2026-06-04 00:10:48 -0500

    Configura DI y DbContext, elimina HTTPS y autorización
    Se agregan los using necesarios, se configura el DbContext con SQL Server y se registran repositorios y servicios en el contenedor de dependencias. Se eliminan las llamadas a UseHttpsRedirection y UseAuthorization. Sin cambios en controladores ni ejecución.

--- ProyectoSolucionP2P.API/Program.cs
+++ ProyectoSolucionP2P.API/Program.cs
@@ -1,6 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoSolucionP2P.CORE.Core.Entities;
+using ProyectoSolucionP2P.CORE.Core.Interfaces;
+using ProyectoSolucionP2P.CORE.Infraestructure.Repositories;
+using ProyectoSolucionP2P.CORE.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// connection string
+var cnx = builder.Configuration.GetConnectionString("DevConnection");
+
+builder.Services.AddDbContext<CambioSeguroP2pdbContext>(options =>
+    options.UseSqlServer(cnx));
+
+// Repositories
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddTransient<IVerificacionIdentidadRepository, VerificacionIdentidadRepository>();
+builder.Services.AddTransient<IMensajeRepository, MensajeRepository>();
+builder.Services.AddTransient<IOfertaRepository, OfertaRepository>();
+builder.Services.AddTransient<IReporteAdministrativoRepository, ReporteAdministrativoRepository>();
+builder.Services.AddTransient<IOperacionRepository, OperacionRepository>();
+builder.Services.AddTransient<ICalificacionRepository, CalificacionRepository>();
+builder.Services.AddTransient<ITemporizadorOperacionRepository, TemporizadorOperacionRepository>();
+builder.Services.AddTransient<IComprobantePagoRepository, ComprobantePagoRepository>();
+builder.Services.AddTransient<IDisputaRepository, DisputaRepository>();
+
+// Services
+builder.Services.AddTransient<IUsuarioService, UsuarioService>();
+builder.Services.AddTransient<IVerificacionIdentidadService, VerificacionIdentidadService>();
+builder.Services.AddTransient<IMensajeService, MensajeService>();
+builder.Services.AddTransient<IOfertaService, OfertaService>();
+builder.Services.AddTransient<IReporteAdministrativoService, ReporteAdministrativoService>();
+builder.Services.AddTransient<IOperacionService, OperacionService>();
+builder.Services.AddTransient<ICalificacionService, CalificacionService>();
+builder.Services.AddTransient<ITemporizadorOperacionService, TemporizadorOperacionService>();
+builder.Services.AddTransient<IComprobantePagoService, ComprobantePagoService>();
+builder.Services.AddTransient<IDisputaService, DisputaService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -13,8 +47,6 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
